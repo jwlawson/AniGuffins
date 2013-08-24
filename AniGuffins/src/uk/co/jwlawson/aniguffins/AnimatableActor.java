8cb6b67e2b455f29343646bb262e58fb9856b3a7
John Lawson <john@jwlawson.co.uk>
2013-08-24 10:46:08 +0100

    Initial commit

---
+++ AniGuffins/src/uk/co/jwlawson/aniguffins/AnimatableActor.java
@@ -0,0 +1,32 @@
+package uk.co.jwlawson.aniguffins;
+
+import java.util.HashMap;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class AnimatableActor extends AnimatedActor {
+
+	private HashMap<String, Animation> mAnimations;
+
+	public AnimatableActor(TextureRegion tex) {
+		super(tex);
+	}
+
+	public void addAnimation(String name, Animation ani) {
+		mAnimations.put(name, ani);
+	}
+
+	protected Animation getAnimation(String name) {
+		if (mAnimations.containsKey(name)) {
+			return mAnimations.get(name);
+		} else {
+			throw new IllegalArgumentException("No animation available with name " + name);
+		}
+	}
+
+	public void activateAnimation(String name, boolean looping) {
+		activateAnimation(getAnimation(name), looping);
+	}
+
+}
